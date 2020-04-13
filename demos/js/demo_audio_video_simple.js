8cc6bb63d7938ea6e01defd38423b648dc7d8829
Anderson <anderson.silva@medilabsistemas.com.br>
2020-04-13 14:55:46 -0300

    V1.2 - Data como parametro na URL,invalidação do link, botões mute e logout

--- demos/js/demo_audio_video_simple.js
+++ demos/js/demo_audio_video_simple.js
@@ -8,15 +8,48 @@ function connect() {
     easyrtc.setRoomOccupantListener(callEverybodyElse);
     easyrtc.easyApp("easyrtc.audioVideoSimple", "selfVideo", ["callerVideo"], loginSuccess, loginFailure);
     CapturaParametrosUrl();
-    easyrtc.setUsername(parametrosUrl.toString());
 
      var chaveValor = parametrosUrl.toString().split(",");
-     //console.log(chaveValor);
+
+     console.log(chaveValor);
 
      conexao[chaveValor[0].substring(0, chaveValor[0].indexOf("="))] = chaveValor[0].substring(chaveValor[0].indexOf("=") + 1);
      conexao[chaveValor[1].substring(0, chaveValor[1].indexOf("="))] = chaveValor[1].substring(chaveValor[1].indexOf("=") + 1);
      conexao[chaveValor[2].substring(0, chaveValor[2].indexOf("="))] = chaveValor[2].substring(chaveValor[2].indexOf("=") + 1);
-     //console.log(conexao);
+     conexao[chaveValor[3].substring(0, chaveValor[3].indexOf("="))] = chaveValor[3].substring(chaveValor[3].indexOf("=") + 1);
+     conexao.NOMEPACIENTE = conexao.NOMEPACIENTE.replace(/%20/g, " ")
+     console.log(conexao.NOMEPACIENTE)
+     validadorConexao = chaveValor[2].concat(chaveValor[3])
+     validadorConexao = conexao.NOMEPACIENTE + chaveValor[2] + chaveValor[3]
+     console.log(validadorConexao)
+     easyrtc.setUsername(validadorConexao);
+     console.log(conexao);
+}
+
+function muteActiveBox() {
+    updateMuteImage(true);
+}
+
+function updateMuteImage(toggle) {
+    //var muteButton = document.getElementById('muteButton');
+    //if( activeBox > 0) { // no kill button for self video
+        //muteButton.style.display = "block";
+        var videoObject = document.getElementById("callerVideo");
+        console.log(videoObject)
+        var isMuted = videoObject.muted?true:false;
+        if( toggle) {
+            isMuted = !isMuted;
+            videoObject.muted = isMuted;
+        }
+        //muteButton.src = isMuted?"images/button_unmute.png":"images/button_mute.png";
+/*     }
+    else {
+        muteButton.style.display = "none";
+    } */
+}
+
+function sair(){
+    window.location.href = "sair.html";
 }
 
 function convertMiliseconds(millisecondVal) {
@@ -48,17 +81,11 @@ function callEverybodyElse(roomName, otherPeople) {
     easyrtc.setRoomOccupantListener(null); // so we're only called once.
     var list = [];
     var connectCount = 0;
-    
+    console.log(roomName)
     for(var easyrtcid in otherPeople ) {
-        if (parametrosUrl.toString() === easyrtc.idToName(easyrtcid)){
-            var intervalo = easyrtc.roomData.default.clientList[selfEasyrtcid].roomJoinTime - easyrtc.roomData.default.clientList[easyrtcid].roomJoinTime;
-
-            if (convertMiliseconds(intervalo) < 30){
-                list.push(easyrtcid);
-                establishConnection(list.length-1);
-            }else{
-                window.location.href = "erro.html";
-            }
+        if (validadorConexao.toString() === easyrtc.idToName(easyrtcid)){
+            list.push(easyrtcid);
+            establishConnection(list.length-1);
         }
     }
     //
@@ -76,16 +103,46 @@ function callEverybodyElse(roomName, otherPeople) {
     }
 }
 
+function converteData(data){
+    var temporario = data.split("DATA=")
+    temporario[1] = new Date(temporario[1])
+    console.log(typeof temporario[1]);
+    return temporario[1]
+}
+
 function loginSuccess(easyrtcid) {
     selfEasyrtcid = easyrtcid;
+    console.log(easyrtc)
+
+    var logar = true;
     var cont = 0
     for(var tagConnection in easyrtc.roomData.default.clientList){
-        if (parametrosUrl.toString() === easyrtc.idToName(tagConnection)){
+        if (validadorConexao.toString() === easyrtc.idToName(tagConnection)){
             cont+=1
+            
+        }
+        horaFinal = new Date()
+        console.log(horaFinal)
+        console.log(Date.parse(horaFinal))
+        
+        var horaInicial = converteData(easyrtc.idToName(tagConnection))
+        console.log(horaInicial)
+        var intervalo = Date.parse(horaFinal) - Date.parse(horaInicial)
+        console.log(horaFinal.toDateString(horaFinal))
+        console.log(horaInicial.toDateString(horaInicial))
+        console.log(intervalo)
+        if (convertMiliseconds(intervalo) < 2 && horaFinal.toDateString(horaFinal) === horaInicial.toDateString(horaInicial)){
+            logar = true
+        }else{
+            logar = false
         }
     }
 
-    if(cont > 2){
+    if(cont > 2 ){
+        easyrtc.disconnect();
+        window.location.href = "erro.html";
+    }
+    if(logar === false ){
         easyrtc.disconnect();
         window.location.href = "erro.html";
     }
